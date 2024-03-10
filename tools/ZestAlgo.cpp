6ca48da97de222a947774fc59f884a20424a1d56
Cheung4843 <46809977+cheung4843@users.noreply.github.com>
2024-03-10 22:12:54 +0800

    add: ZestAlgo.cpp

---
+++ tools/ZestAlgo.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "d88888D d88888b .d8888. d888888b  .d8b.  db       d888b   .d88b.  " << endl;
+    cout << "YP  d8' 88'     88'  YP `~~88~~' d8' `8b 88      88' Y8b .8P  Y8. " << endl;
+    cout << "   d8'  88ooooo `8bo.      88    88ooo88 88      88      88    88 " << endl;
+    cout << "  d8'   88~~~~~   `Y8b.    88    88~~~88 88      88  ooo 88    88 " << endl;
+    cout << " d8' db 88.     db   8D    88    88   88 88booo. 88. ~8~ `8b  d8' " << endl;
+    cout << "d88888P Y88888P `8888Y'    YP    YP   YP Y88888P  Y888P   `Y88P'  " << endl;
+
+    return 0;
+}
